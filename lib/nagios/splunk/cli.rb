2baa98b0fdd7bf0cec3dfc59c3a41a344236c39a
Max Horbul <max.horbul@livingsocial.com>
2012-04-12 09:43:34 -0700

    move logic inside CLI class and make executable cleaner

--- lib/nagios/splunk/cli.rb
+++ lib/nagios/splunk/cli.rb
@@ -9,20 +9,33 @@ module Nagios
       option(:server_url,
              :short => "-s URL",
              :long  => "--server URL",
-             :default => 'https://admin:changeme@localhost:8089/',
-             :description => "Splunk server url")
+             :required => true,
+             :description => "Splunk server url",
+             :on => :head)
 
       option(:warn,
              :short => "-w WARN",
              :long  => "--warn WARN",
              :default => '80',
-             :description => "Warn % of license capacity usage")
+             :description => "Warn % of license capacity usage (default: 80)")
 
       option(:crit,
              :short => "-c CRIT",
              :long  => "--crit CRIT",
              :default => '90',
-             :description => "Critical % of license capacity usage")
+             :description => "Critical % of license capacity usage (default: 90)")
+
+      def run(argv = ARGV)
+        parse_options(argv)
+
+        client = RestClient.new(config[:server_url])
+        splunk = Check.new(client)
+
+        status, message = splunk.license_usage(config[:warn], config[:crit])
+
+        puts message
+        status
+      end
     end
 
   end
