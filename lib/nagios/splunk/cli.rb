622a696fd127278cadd7b8beb072ee83d786d1f5
Max Horbul <max@gorbul.net>
2014-01-30 16:13:05 -0800

    new command line arguments -L allows to run localslave check

--- lib/nagios/splunk/cli.rb
+++ lib/nagios/splunk/cli.rb
@@ -30,6 +30,11 @@ module Nagios
              :long  => "--pool-name POOL",
              :description => "Pool name wich usage is being checked.")
 
+      option(:localslave,
+             :short => "-L",
+             :long  => "--localslave-check",
+             :description => "Run localslave check.")
+
       option(:stack_id,
              :short => "-s STACK_ID",
              :long  => "--stack-id STACK_ID",
@@ -43,7 +48,9 @@ module Nagios
         splunk = Check.new(client)
 
         begin
-          if config[:pool]
+          if config[:localslave]
+            status, message = splunk.localslave(config[:warn], config[:crit])
+          elsif config[:pool]
             status, message = splunk.pool_usage(config[:pool], config[:warn], config[:crit])
           else
             status, message = splunk.license_usage(config[:warn], config[:crit], config[:stack_id])
