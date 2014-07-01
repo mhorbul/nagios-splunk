9b64c8e423ae5279f00e1f68f377275d1c954c3f
Max Horbul <max@gorbul.net>
2014-07-01 14:21:50 +0300

    convert response code into Number before match it

--- lib/nagios/splunk/splunk.rb
+++ lib/nagios/splunk/splunk.rb
@@ -10,12 +10,12 @@ module Nagios
 
       def cluster_master_info
         response = @client.get("/services/cluster/master/info")
-        Nokogiri::Slop.new(response.body) if response.code == 200
+        Nokogiri::Slop(response.body) if response.code.to_i == 200
       end
 
       def cluster_master_generation
         response = @client.get("/services/cluster/master/generation")
-        Nokogiri::Slop.new(response.body) if response.code == 200
+        Nokogiri::Slop(response.body) if response.code.to_i == 200
       end
 
     end
